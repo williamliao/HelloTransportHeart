e60859b00b0b06ba115053f2575d3981c6c25ff1
william.liao <william.liao@mitake.com.tw>
2022-01-10 10:19:50 +0800

    add LocationUpdater

--- HelloTransportHeart/Share/LocationUpdater.swift
+++ HelloTransportHeart/Share/LocationUpdater.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import CoreLocation
+
+@MainActor
+class LocationUpdater: NSObject, CLLocationManagerDelegate {
+    private(set) var authorizationStatus: CLAuthorizationStatus
+    
+    private let locationManager: CLLocationManager
+    
+    // The continuation we will use to asynchronously ask the user permission to track their location.
+    private var permissionContinuation: CheckedContinuation<CLAuthorizationStatus, Never>?
+    
+    var locationHandler: ([CLLocation]) -> Void = { _ in }
+    
+    override init() {
+        locationManager = CLLocationManager()
+        authorizationStatus = locationManager.authorizationStatus
+        
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func start() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stop() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func requestPermission() async -> CLAuthorizationStatus {
+        locationManager.requestWhenInUseAuthorization()
+        return await withCheckedContinuation { continuation in
+            permissionContinuation = continuation
+        }
+    }
+    
+    // MARK: - Location Delegate
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationHandler(locations)
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+        permissionContinuation?.resume(returning: authorizationStatus)
+    }
+}
