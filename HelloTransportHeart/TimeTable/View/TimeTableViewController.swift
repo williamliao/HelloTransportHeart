54499bd4e7dee21b70c9ac235f538fbb0e3dcaf1
william.liao <william.liao@mitake.com.tw>
2022-01-10 16:06:07 +0800

    add timeTableView

---
+++ HelloTransportHeart/TimeTable/View/TimeTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TimeTableViewController.swift
+//  HelloTransportHeart
+//
+//  Created by 雲端開發部-廖彥勛 on 2022/1/10.
+//
+
+import UIKit
+
+class TimeTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
